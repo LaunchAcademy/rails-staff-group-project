3d63b81ee5176627c8a2f28d09dc4567ad06940f
ohad-porat <ohadp23@gmail.com>
2021-09-21 11:55:32 -0400

    add post comment feature

--- app/javascript/react/components/layout/ResourceShow.js
+++ app/javascript/react/components/layout/ResourceShow.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react"
 
 import CommentTile from "./CommentTile"
+import CommentForm from "./CommentForm"
 
 const ResourceShow = (props) => {
-  const [resource, setResource] = useState({})
-  const [comments, setComments] = useState([])
+  const [resource, setResource] = useState({ comments: [] })
+
   const resourceId = props.match.params.id
 
   const fetchResource = async () => {
@@ -17,7 +18,6 @@ const ResourceShow = (props) => {
       }
       const resourceData = await response.json()
       setResource(resourceData.resource)
-      setComments(resourceData.resource.comments)
     } catch (error) {
       console.error(`Error in fetch: ${error.message}`)
     }
@@ -27,16 +27,43 @@ const ResourceShow = (props) => {
     fetchResource()
   }, [])
 
-  const commentTiles = comments.map((comment) => {
+  const addNewComment = async (comment) => {
+    try {
+      const response = await fetch(`/api/v1/resources/${resourceId}/comments`, {
+        credentials: "same-origin",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(comment),
+      })
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error
+      }
+      const responseBody = await response.json()
+      const newComments = resource.comments.concat(responseBody.comment)
+      setResource({ ...resource, comments: newComments })
+    } catch (error) {
+      console.error(`Error in Fetch: ${error.message}`)
+    }
+  }
+
+  const commentTiles = resource.comments.map((comment) => {
     return <CommentTile key={comment.id} comment={comment} />
   })
 
   return (
     <div>
       <h1>{resource.name}</h1>
-      <h4><a href={resource.url} target="_blank">
-        {resource.url}
-      </a></h4>
+      <h4>
+        <a href={resource.url} target="_blank">
+          {resource.url}
+        </a>
+      </h4>
+      <CommentForm addNewComment={addNewComment} />
       {commentTiles}
     </div>
   )
